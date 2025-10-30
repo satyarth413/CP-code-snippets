1ac15520afc90e07270c9859781090dca596bb41
Satyarth Pandey <satyarth2002pandey@gmail.com>
2025-10-30 22:38:43 +0530

    Add Sublime Text Configs and pragma change which work for c++23

--- Starter code templates/run_against_correct_code.cpp
+++ Starter code templates/run_against_correct_code.cpp
@@ -1,5 +1,5 @@
 // #pragma GCC optimize("Ofast")
-// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+// #pragma GCC target("sse,sse2,sse3,mmx")
 // #pragma GCC optimize("unroll-loops")
 
 #include <bits/stdc++.h>
